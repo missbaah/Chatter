d0acf1e24cf952891d4fb408e7b60f45f01c4b76
Adwoa Addo-Brako <baahbrako@gmail.com>
2023-06-01 15:36:47 +0000

    defining routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home, LogIn, SignUp } from "./routes";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/login" element={<SignUp />} />
+        <Route path="/signup" element={<LogIn />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
